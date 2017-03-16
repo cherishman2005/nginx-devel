e777689f6581a3bdaf3d068910fd58dc016e7683
cherishman2005 <cherishman2005@163.com>
2017-03-17 00:19:33 +0800

    openresty-mysql

--- nginx-1.11.6/add_module/ngx_http_out_module/README.md
+++
@@ -1,4 +0,0 @@
-ngx_http_out_module
-===================
-
-a simple nginx module like hello_world
--- openresty-1.11.2.2/make_auto.sh
+++
@@ -1,9 +0,0 @@
-
-./configure --prefix=/usr/local/openresty --with-debug --with-http_v2_module --with-stream \
-        --with-openssl=/home/nginx/openssl-1.0.2k \
-
- 
-make  &&  make install
-
-exit 0
-
